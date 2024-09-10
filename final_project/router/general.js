7a1d584f8d9b8794974c50cf01dd8333328f0072
“ricard0g” <ricardoguzman444@gmail.com>
2024-09-10 12:31:18 -0400

    /isbn/:isbn endpoint fetches and returns book details using promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,8 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require("axios");
+const http = require("http");
 
 const doesExist = (username) => {
   let usersWithSameName = users.filter((user) => user.username === username);
@@ -25,16 +27,58 @@ public_users.post("/register", (req, res) => {
   return res.status(404).json({ message: "Unable to register user" });
 });
 
+// For the task 10. A simulated API endpoint to make the request and then return the books DB
+const mockApiUrl = "http://localhost:5000/api/books";
+
+public_users.get("/api/books", (req, res) => {
+  res.json(books);
+});
+
 // Get the book list available in the shop
-public_users.get("/", function (req, res) {
-  //Write your code here
-  return res.status(200).send(JSON.stringify(books, null, 4));
+public_users.get("/", async function (req, res) {
+  try {
+    const response = await axios.get(mockApiUrl);
+    if (response) {
+      return res.status(200).json(response.data);
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
   const { isbn } = req.params;
-  return res.status(200).send(JSON.stringify(books[isbn], null, 4));
+  const getBooksByIsbn = (url) => {
+    return new Promise((resolve, reject) => {
+      http
+        .get(url, (response) => {
+          let data = "";
+
+          response.on("data", (chunk) => {
+            data += chunk;
+          });
+
+          response.on("end", () => {
+            resolve(JSON.parse(data));
+          });
+        })
+        .on("error", (error) => {
+          reject(error);
+        });
+    });
+  };
+
+  // We make the request to the mockUrl to get the books object. Parsed into an objct from the JSON response we get on the promise
+  getBooksByIsbn(`${mockApiUrl}`)
+    .then((data) => {
+      return res.status(200).send(data[isbn]);
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.status(500).json({ message: "Error fetching book data" });
+    });
 });
 
 // Get book details based on author
